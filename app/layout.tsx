52ac82c9212a970f4769a4ff60d24cadaaa66c6e
rustam <kerimov.rustam@live.ru>
2024-07-31 21:47:17 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,27 @@
 import React, { ReactNode } from 'react'
 import '@/assets/styles/globals.css'
 
-import { Typography } from "@material-tailwind/react";
+import NavBar from '@/components/Header/Navbar';
+import Banner from '@/components/Header/Banner';
+
 export const metadata = {
   title: 'Home',
   keywords: 'LWR'
 }
 
+import LWRFooter from '@/components/Footer/LWRFooter'
 
 
 const HomeLayout: React.FC<({ children: ReactNode })> = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <Typography>
-          Material Tailwind is an easy to use components library for Tailwind CSS
-          and Material Design. It provides a simple way to customize your
-          components, you can change the colors, fonts, breakpoints and everything
-          you need.
-        </Typography>
+        <Banner />
+        <NavBar />
+        <main>
+          {children}
+        </main>
+        <LWRFooter />
       </body>
     </html>
   )
--- dummy.tsx
+++
@@ -1,9 +0,0 @@
-const rates = {
-    weekly: 10,
-}
-{ rates.monthly !== undefined ? `$${rates.monthly}/mo` : rates.weekly !== undefined ? `$${rates.weekly}/wk` : '' }
-
-
-{
-
-}
