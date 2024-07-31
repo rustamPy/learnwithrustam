86f7357d8e67a391ba76102f3e36117c8aedccd7
rustam <kerimov.rustam@live.ru>
2024-07-31 21:27:47 +0200

    stage - temp

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 import React, { ReactNode } from 'react'
 import '@/assets/styles/globals.css'
 
-import NavBar from '@/components/Header/Navbar';
-import Banner from '@/components/Header/Banner';
 import { Typography } from "@material-tailwind/react";
 export const metadata = {
   title: 'Home',
   keywords: 'LWR'
 }
 
-import LWRFooter from '@/components/Footer/LWRFooter'
-import SearchWindow from '@/components/Hero/SearchWindow';
 
 
 const HomeLayout: React.FC<({ children: ReactNode })> = ({ children }) => {
