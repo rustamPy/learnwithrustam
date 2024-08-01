02428edb4efd836451f9c8313751f6dd46111490
rustam <kerimov.rustam@live.ru>
2024-08-02 00:49:48 +0200

    fix

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { getServerSession } from "next-auth/next"
-import { authOptions } from "./api/auth/[...nextauth]/route"
+import { authOptions } from "@/app/api/auth/[...nextauth]/options"
 import IndexHero from '@/components/Hero/IndexHero';
 
 const Index = async () => {
