8f78b7a65cd94d0a305f384293f417c3aff25451
rustam <kerimov.rustam@live.ru>
2024-08-02 01:03:46 +0200

    remove logged in message

--- app/page.js
+++ app/page.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { getServerSession } from "next-auth/next"
-import { authOptions } from "@/app/api/auth/[...nextauth]/options"
 import IndexHero from '@/components/Hero/IndexHero';
 
 const Index = async () => {
-  const session = await getServerSession(authOptions)
 
   return (
     <>
-      {session ? 'Logged in' : 'Not logged in'}
       <IndexHero />
     </>
   )
