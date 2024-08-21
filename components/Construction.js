ebcbc87e8c6e4338e21d365ec14ba9c5f66bdb3c
rustam <kerimov.rustam@live.ru>
2024-08-21 15:44:33 +0200

    beta 0.4.4.2 - cosmetic

--- components/Construction.js
+++ components/Construction.js
@@ -1,4 +1,5 @@
 import { Typography } from '@material-tailwind/react';
+import { LuConstruction } from "react-icons/lu";
 
 
 const Construction = () => {
@@ -7,10 +8,13 @@ const Construction = () => {
             <div className="backdrop-blur-lg absolute inset-0"></div>
             <Typography
                 color="red"
-                className="text-6xl font-extrabold uppercase text-center z-20"
+                className="text-4xl font-extrabold uppercase text-center z-20 flex flex-col items-center"
             >
                 Under Construction
+
+                <LuConstruction size={60} />
             </Typography>
+
         </div>
     )
 }
