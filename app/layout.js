7823ef01822b80bcc53d714217b06f9d791c7d25
rustam <kerimov.rustam@live.ru>
2024-08-23 22:56:04 +0200

    beta 0.4.6 - updated favicon

--- app/layout.js
+++ app/layout.js
@@ -1,31 +1,35 @@
-import React from 'react'
-import '@/assets/styles/globals.css'
-import { getServerSession } from "next-auth/next"
-import { authOptions } from "@/app/api/auth/[...nextauth]/options"
-import ClientSessionProvider from './SessionProvider'
+import React from 'react';
+import '@/assets/styles/globals.css';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/app/api/auth/[...nextauth]/options';
+import ClientSessionProvider from './SessionProvider';
 import NavBar from '@/components/Header/Navbar';
-import LWRFooter from '@/components/Footer/LWRFooter'
-
+import LWRFooter from '@/components/Footer/LWRFooter';
+import Head from 'next/head';
 
 export default async function RootLayout({ children }) {
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 
   return (
     <html lang="en">
-      <head>
+      <Head>
+        <title>LWR</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
-      </head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <ClientSessionProvider session={session}>
           <NavBar />
-          <main>
-            {children}
-          </main>
+          <main>{children}</main>
           <LWRFooter />
         </ClientSessionProvider>
       </body>
     </html>
-  )
+  );
 }
